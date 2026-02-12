49cfcb6826a7cbad2b7017b3f0b8b9bd5ba4403f
ShaeTsuPog <shaetsu@protonmail.com>
2026-02-12 04:36:29 -0800

    zed update

--- .config/fish/functions/apng.fish
+++ .config/fish/functions/apng.fish
@@ -14,7 +14,6 @@ function apng
         ffmpeg -i "$input" -vf "scale='if(gt(iw,ih),220,-1)':'if(gt(iw,ih),-1,220)':flags=lanczos" \
         -f apng -plays 0 -pred avg -pix_fmt rgba "$output"
     else
-        echo "Usage: gif2apng [file.gif] or gif2apng -a"
+        echo "Usage: apng [file.gif] or apng -a"
     end
 end
-
