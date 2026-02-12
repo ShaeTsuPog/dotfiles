236d62525dc8881801de03d7ce11befabc405dba
ShaeTsuPog <shaetsu@protonmail.com>
2026-02-12 08:30:12 -0800

    fish indenting fixed?

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,12 @@
 if status is-interactive
-  starship init fish | source
-	fzf --fish | source
-	zoxide init fish --cmd cd | source
 
-	set -gx FZF_DEFAULT_OPTS_FILE "$HOME/.config/fzf/vague.conf"
-	set -gx FZF_DEFAULT_COMMAND "fd --type f --strip-cwd-prefix --hidden --exclude .git -x eza --icons=always --color=always {}"
-	set -gx FZF_DEFAULT_OPTS "--ansi"
+  starship init fish | source
+  fzf --fish | source
+  zoxide init fish --cmd cd | source
+
+  set -gx FZF_DEFAULT_OPTS_FILE "$HOME/.config/fzf/vague.conf"
+  set -gx FZF_DEFAULT_COMMAND "fd --type f --strip-cwd-prefix --hidden --exclude .git -x eza --icons=always --color=always {}"
+  set -gx FZF_DEFAULT_OPTS "--ansi"
   set -gx STARSHIP_CONFIG ~/.config/starship/starship.toml
   set -gx EZA_COLORS "uu=35:da=33"
 
